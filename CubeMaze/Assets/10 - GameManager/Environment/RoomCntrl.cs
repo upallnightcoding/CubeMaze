bde033fec7371268299f61990e9c984f925a9afc
upallnightcoding <upallnightcoding@gmail.com>
2025-03-20 01:42:41 -0400

    Add Lerp to rotation

--- CubeMaze/Assets/10 - GameManager/Environment/RoomCntrl.cs
+++ CubeMaze/Assets/10 - GameManager/Environment/RoomCntrl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,13 +11,33 @@ public class RoomCntrl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(RotateObject(point, axisX, 90.0f, 1.0f));
     }
 
     // Update is called once per frame
     void Update()
     {
         //transform.Rotate(axisX, 0.25f);
-        transform.RotateAround(point, axisX, 0.1f);
+        //transform.RotateAround(point, axisX, 0.1f);
+    }
+
+    private IEnumerator RotateObject(Vector3 point, Vector3 axis, float rotateAmount, float rotateTime )
+    {
+        float step = 0.0f; //non-smoothed
+        var rate = 1.0 / rotateTime; //amount to increase non-smooth step by
+        var smoothStep = 0.0; //smooth step this time
+        var lastStep = 0.0; //smooth step last time
+
+        while (step < 1.0)
+        { 
+            step += (float)(Time.deltaTime * rate); 
+            smoothStep = Mathf.SmoothStep(0.0f, 1.0f, step); 
+            transform.RotateAround(point, axis, (float)(rotateAmount * (smoothStep - lastStep)));
+            lastStep = smoothStep; 
+
+            yield return null; 
+        }
+        
+        if (step > 1.0) transform.RotateAround(point, axis, (float) (rotateAmount * (1.0 - lastStep)));
     }
 }
